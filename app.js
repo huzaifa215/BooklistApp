6361a408c26b6737c37e093e3e4254382e6bffd0
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-07-25 20:29:45 +0500

    custom alert of add delete and empty

--- app.js
+++ app.js
@@ -53,6 +53,16 @@ class UI{
         }
 
     }
+    static showAlert(message,className){
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
 
 static clearFeilds(){
     document.querySelector('#title').value='';
@@ -66,7 +76,6 @@ static clearFeilds(){
 document.addEventListener('DOMContentLoaded',UI.displayBooks);
 
 // add a book
-
 document.querySelector('#book-form').addEventListener('submit',(e)=>{
     // defullt action
     e.preventDefault();//to stop it
@@ -75,20 +84,29 @@ document.querySelector('#book-form').addEventListener('submit',(e)=>{
      const tittle=document.querySelector('#title').value;
      const author=document.querySelector('#author').value;
      const isbn=document.querySelector('#isbn').value; 
-
+    // validation 
+    if(tittle === '' || author === '' ||  isbn=== ''){
+        UI.showAlert("Please fill the Feilds",'danger');
+    }
+    else{
+       
      // instance a book
      const book =new Book(tittle,author,isbn);
      console.log(book);
   //   alert("Book Added");
      UI.addBookToList(book);
+     // show successful msg
+     UI.showAlert("BOOK Added Successfully",'success');
      // clear feilds
      UI.clearFeilds();
+    }
 });
 
 
 // remove event 
 document.querySelector('#book-list').addEventListener('click',(e)=>{
     UI.deleteBook(e.target);
+    UI.showAlert("BOOK Deleted Successfully",'success');
 });
 
 
